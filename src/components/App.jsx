f09d523a76a3783a0aaa7039edc38e306de10faf
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2023-07-30 15:39:50 +0530

    project setup

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import "../styles/src.css";
+import "../styles/custom.css";
+
+function App() {
+  return (
+    <div className="test-div text-white text-2xl tracking-wider bg-blue-400 flex justify-center font-bold">
+      Hello world!
+    </div>
+  );
+}
+
+export default App;
