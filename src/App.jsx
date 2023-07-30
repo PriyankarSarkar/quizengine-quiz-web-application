f46fd77f91216caa8ac9b0c3fd3901035ddbce83
Priyankar Sarkar <priyankarsarkar2020@gmail.com>
2023-07-30 15:08:19 +0530

    backup commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="test-div  cust-bg-yellow  text-white text-2xl tracking-wider">Hello world!</div>
+    </>
+  )
+}
+
+export default App
